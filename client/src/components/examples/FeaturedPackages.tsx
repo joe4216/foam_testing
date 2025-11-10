5dc56e82822e6ed6f17e18bca699f45b7b6e6c30
joe4216 <94731565+joe4216@users.noreply.github.com>
2025-11-09 23:58:34 -0600

    Delete client directory

--- client/src/components/examples/FeaturedPackages.tsx
+++
@@ -1,5 +0,0 @@
-import FeaturedPackages from '../FeaturedPackages'
-
-export default function FeaturedPackagesExample() {
-  return <FeaturedPackages onBookClick={(pkg) => console.log('Book:', pkg)} />
-}
